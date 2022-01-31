b179ec8a09dd7da5d4f7b943b417098c472193df
Debadrita Ghosh <debadrita.ghosh2010@gmail.com>
2022-01-31 21:44:36 +0530

    Day2-middleware-Creating and Using custon middleware

--- Day2-Middleware/index.js
+++ Day2-Middleware/index.js
@@ -6,7 +6,18 @@ const app = express();
 //Using morgan function which will return a middleware
 //app.use(morgan('dev')); //Global middleware
 
-app.get("/",morgan("dev"),(req,res) => {
+//Creating custon middleware
+function custonmMiddleware(req,res,next) {
+    if(req.url === "/"){
+        res.send("<h1> This is home page!! </h1>")
+    }
+    next();
+}
+
+//Using custom middleware
+app.use(custonmMiddleware);
+
+app.get("/",morgan("dev"),(req,res) => { //Using middleware for single route
     res.send("Hello World!");
 });
 
