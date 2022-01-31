e4564b0dd54bfdbb1cee6d13ba486c2513bc0678
Debadrita Ghosh <debadrita.ghosh2010@gmail.com>
2022-01-31 21:24:31 +0530

    Day2-middleware-Using global middleware

---
+++ Day2-Middleware/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require('morgan');
+
+const app = express();
+
+//Using morgan function which will return a middleware
+app.use(morgan('dev')); //Global middleware
+
+app.get("/",(req,res) => {
+    res.send("Hello World!");
+});
+
+app.get("/about",(req,res) => {
+    res.send("About Page");
+});
+
+app.listen(5000,() => {
+    console.log("Server running!!...");
+})
